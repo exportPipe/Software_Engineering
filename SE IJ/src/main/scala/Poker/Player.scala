6b426f427ddb8468064c6e1d2b15b109eb8a6fd4
exportPipe <paulruess97@gmail.com>
2020-05-14 15:06:57 +0200

    added String representation

--- SE IJ/src/main/scala/Poker/Player.scala
+++
@@ -1,9 +0,0 @@
-package Poker
-
-case class Player(cred: Int, active: Boolean) {
-  val num = Player.newNum
-} //END OF PLAYER CLASS
-object Player {
-  private var idNum = 0
-  private def newNum = { idNum += 1 }
-} //END OF COMPANION OBJECT PLAYER
