f5f7ad37887707ba8dadb524da01d7b522bccb8b
exportPipe <paulruess97@gmail.com>
2020-05-15 10:24:19 +0200

    added "Card"

--- SE IJ/src/main/scala/Poker/Poker.scala
+++ SE IJ/src/main/scala/Poker/Poker.scala
@@ -4,8 +4,13 @@ object Poker {
   def main(args: Array[String]): Unit = {
     println("Texas Holdem Poker")
 
+    // String representation of a card
+    val testcard = Card(2, 4)
+    println(testcard.toString)
+    // String representation of a deck
     val testdeck = new CardDeck
-    testdeck.fillDeck
+    testdeck.fillDeck()
+
 
 
 
