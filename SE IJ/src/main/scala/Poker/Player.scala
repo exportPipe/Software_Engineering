936bdbecb06be26d7e162ea65b7468b5f7ced633
exportPipe <paulruess97@gmail.com>
2020-05-15 13:48:42 +0200

    added "Player"

--- SE IJ/src/main/scala/Poker/Player.scala
+++ SE IJ/src/main/scala/Poker/Player.scala
@@ -1,21 +1,10 @@
-package scala.Poker
+package Poker
 
-
-case class Player() {
-  var num = 0
-  var cred = 0
-  var active = true
-
-  def setNumber(num: Int): Unit = {
-    this.num = num
-  } //END OF SETNUMBER
-
-  def setCredit(cred: Int): Unit = {
-    this.cred = cred
-  } //END OF SETCREDIT
-
-  def setActive(active: Boolean){
-    this.active = active
-  } //END OF SETACTIVE
-
-} //END OF PLAYER CLASS
+case class Player(credit: Int = 500, isActive: Boolean = true) {
+  val id: Int = Player.newID
+}
+object Player {
+  private var idNum = 0
+  private def newID: Int = {idNum += 1
+    idNum}
+}
