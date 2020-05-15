68763ac5b46607a93c771fe88a3b061a9511c427
exportPipe <paulruess97@gmail.com>
2020-05-15 13:12:09 +0200

    added Card(Deck) functionality

--- SE IJ/src/main/scala/Poker/CardDeck.scala
+++ SE IJ/src/main/scala/Poker/CardDeck.scala
@@ -2,12 +2,28 @@ package Poker
 
 case class CardDeck() {
 
-  val deck: IndexedSeq[Card] = for {
-    rank <- 1.to(13)
-    suit <- 1.to(4)
+  val deckSeq: IndexedSeq[Card] = for {
+    suit <- 1 to 4
+    rank <- 1 to 13
   } yield Card(rank, suit)
 
-  def restDeck = 0
+  var deck: Array[Card] = Array.from(deckSeq)
+  var restDeck = 0
 
+  def mixCards(): Unit = {
+    val random = scala.util.Random
+    for(i <- 0 to 100) {
+      val r1 = random.nextInt(52)
+      val r2 = random.nextInt(52)
+      val tmp = deck(r1)
+      deck(r1) = deck(r2)
+      deck(r2) = tmp
+    }
+  }
+
+  def drawCard(): Card = {
+    restDeck += 1
+    deck(restDeck - 1)
+  }
 }
 
--- SE IJ/src/main/scala/Poker/Poker.scala
+++ SE IJ/src/main/scala/Poker/Poker.scala
@@ -4,13 +4,11 @@ object Poker {
   def main(args: Array[String]): Unit = {
     println("Texas Holdem Poker")
 
-    // String representation of a card
-    val testcard = Card(2, 4)
-    println(testcard.toString)
     // String representation of a deck
     val testdeck = new CardDeck
-    for(i <- 0.to(51)) {
-      println(testdeck.deck(i))
-    }
+    testdeck.mixCards()
+    for (i <- 0 to 51)
+      println(testdeck.drawCard().toString)
+
   }
 }
