2460b4aa7f2850017d121283c78cfea9d1822933
exportPipe <paulruess97@gmail.com>
2020-05-15 09:37:18 +0200

    added "Card"

--- SE IJ/src/main/scala/Poker/CardDeck.scala
+++ SE IJ/src/main/scala/Poker/CardDeck.scala
@@ -3,12 +3,13 @@ package Poker
 case class CardDeck() {
 
   def deck: Array[Card] = new Array[Card](52)
-  def restDeck = 0;
+  def restDeck = 0
 
-  def fillDeck: Unit = {
+  def fillDeck(): Unit = {
     var count = 0
     for (x <- 1.to(13)) {
       for (y <- 1.to(4)) {
+
         print(x, y)
         deck(count) = Card(x, y)
       }
