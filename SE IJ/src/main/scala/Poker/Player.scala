4362006c45539da5b67f696288dfbcf7ee662a0b
StrittmatterN <Niklas-strittmatter@gmx.de>
2020-05-15 11:51:22 +0200

    update Player class

--- SE IJ/src/main/scala/Poker/Player.scala
+++ SE IJ/src/main/scala/Poker/Player.scala
@@ -1,8 +1,21 @@
 package Poker
 
-object Player {
-  def printName(): Unit = {
-    println("Paul!")
-    println("Niklas")
-  }
-}
+
+case class Player() {
+  var num = 0
+  var cred = 0
+  var active = true
+
+  def setNumber(num: Int): Unit = {
+    this.num = num
+  } //END OF SETNUMBER
+
+  def setCredit(cred: Int): Unit = {
+    this.cred = cred
+  } //END OF SETCREDIT
+
+  def setActive(active: Boolean){
+    this.active = active
+  } //END OF SETACTIVE
+
+} //END OF PLAYER CLASS
